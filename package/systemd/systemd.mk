1101c0591a7e71cbb24a989e4ec2d4294a0badb4
Gabe Evans <gabe@hashrabbit.co>
2015-11-03 22:24:30 +0000

    systemd: disable ldconfig through configure option
    Systemd v216+ includes a configure option to disable ldconfig.
    
    Signed-off-by: Gabe Evans <gabe@hashrabbit.co>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- package/systemd/systemd.mk
+++ package/systemd/systemd.mk
@@ -40,6 +40,7 @@ SYSTEMD_CONF_OPTS += \
 	--enable-split-usr \
 	--disable-efi \
 	--disable-gnuefi \
+	--disable-ldconfig \
 	--disable-tests \
 	--disable-dbus \
 	--without-python
@@ -152,17 +153,10 @@ define SYSTEMD_SANITIZE_PATH_IN_UNITS
 		-exec $(SED) 's,$(HOST_DIR),,g' {} \;
 endef
 
-# Disable ldconfig.service, as /sbin/ldconfig is not available when the
-# target is built with a glibc-based toolchain.
-define SYSTEMD_DISABLE_LDCONFIG_SERVICE_HOOK
-	rm -f $(TARGET_DIR)/lib/systemd/system/sysinit.target.wants/ldconfig.service
-endef
-
 SYSTEMD_POST_INSTALL_TARGET_HOOKS += \
 	SYSTEMD_INSTALL_INIT_HOOK \
 	SYSTEMD_INSTALL_MACHINEID_HOOK \
 	SYSTEMD_INSTALL_RESOLVCONF_HOOK \
-	SYSTEMD_DISABLE_LDCONFIG_SERVICE_HOOK \
 	SYSTEMD_SANITIZE_PATH_IN_UNITS
 
 define SYSTEMD_USERS
